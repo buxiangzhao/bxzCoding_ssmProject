dbf2c3e88d268962eb83873faf08cc34b5bf138f
Buxiangzhao <1016862070@qq.com>
2018-06-10 10:56:14 +0800

    添加logback

--- src/main/java/com/ssm/controller/HelloController.java
+++ src/main/java/com/ssm/controller/HelloController.java
@@ -4,15 +4,17 @@ import com.ssm.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
-public class HelloController {
+public class HelloController extends BaseController{
 
     @Autowired
     HelloService helloService;
 
     @RequestMapping("/hello")
     public  String hello(){
+        logger.debug("debug-输出");
+        logger.info("info-输出");
+        logger.error("error-输出");
         helloService.helloSave();
         return "hello";
     }
--- src/main/java/com/ssm/service/impl/HelloServiceImpl.java
+++ src/main/java/com/ssm/service/impl/HelloServiceImpl.java
@@ -5,6 +5,7 @@ import com.ssm.entity.User;
 import com.ssm.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by SQ_BXZ on 2018-06-08.
@@ -14,6 +15,8 @@ public class HelloServiceImpl implements HelloService {
 
 	@Autowired
 	UserMapper userMapper;
+
+	@Transactional
 	@Override
 	public String helloSave() {
 		userMapper.save(new User().setId(1).setUserName("张三").setAge("243岁"));
