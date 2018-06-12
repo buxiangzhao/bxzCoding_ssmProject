f00c6099f6f399d8d65c62fefc6c2d16db4e0640
buxiangzhao <1016862070@qq.com>
2018-06-12 09:54:58 +0800

    添加Junit Test

---
+++ src/test/java/com/ssm/other/LocalTest.java
@@ -0,0 +1,14 @@
+package com.ssm.other;
+
+import org.junit.Test;
+
+/**
+ * Created by SQ_BXZ on 2018-06-12.
+ */
+public class LocalTest {
+
+	@Test
+	public void  test_01() {
+		System.out.println("ddddddd");
+	}
+}
